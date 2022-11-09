b30bd9bffd09a9bca2b055b770f0dc24cebce25e
1lee <1lee.burrell@gmail.com>
2022-11-09 18:21:21 -0500

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,26 @@
+<template>
+	<main>
+
+		<Footer />
+	</main>
+</template>
+
+<script>
+	import Footer from '@/components/Footer.vue'
+
+
+	
+export default {
+	components: {
+		Footer
+	}
+	
+}
+
+
+
+</script>
+
+<style scoped>
+
+</style>
