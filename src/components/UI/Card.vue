0df9024a25427106caf1bbf78a048dd578178512
1lee <1lee.burrell@gmail.com>
2022-11-09 18:48:13 -0500

    basic card hashed out

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="max-w-sm rounded overflow-hidden shadow-lg">
+		<h1 class="absolutew-full bg-white font-bold text-xl mb-2 text-center">
+			{{ title }}
+		</h1>
+
+		<img class="w-full" :src="imgUrl" alt="weather" />
+		<div class="px-6 py-4">
+			<p class="text-gray-700 text-base">
+				{{ mainText }}
+			</p>
+		</div>
+		<div class="px-6 pt-4 pb-2">
+			<span
+				class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+				>test</span
+			>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Card',
+		props: {
+			imgUrl: String,
+			title: String,
+			mainText: String
+		}
+	};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,24 @@
 <template>
 	<main>
-
+		<Card
+			img-url="https://live.staticflickr.com/83/235805526_ebd124b0a8_b.jpg"
+			title="Weather Demo"
+			main-text="Welcome to the weather app demo!"
+		/>
 		<Footer />
 	</main>
 </template>
 
 <script>
-	import Footer from '@/components/Footer.vue'
+	import Card from '@/components/UI/Card.vue';
+	import Footer from '@/components/Footer.vue';
 
-
-	
-export default {
-	components: {
-		Footer
-	}
-	
-}
-
-
-
+	export default {
+		components: {
+			Footer,
+			Card
+		}
+	};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
