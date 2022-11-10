d611df4305e89e372ad902e72d2c04ab9d2b6064
1lee <1lee.burrell@gmail.com>
2022-11-09 20:51:39 -0500

    changed lat/long api

--- src/mixins/fetchWeather.js
+++ src/mixins/fetchWeather.js
@@ -1,41 +1,57 @@
-export const getWeather = address => {
-	var geocoder = new google.maps.Geocoder();
-	let lat, long;
+const resolveCoords = city => {
+	return new Promise(function (res, rej) {
+		let lat, long;
 
-	geocoder.geocode({ address: address }, function (results, status) {
-		if (status == google.maps.GeocoderStatus.OK) {
-			lat = results[0].geometry.location.lat();
-			long = results[0].geometry.location.lng();
-		}
-	});
-
-	fetch(
-		`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&hourly=temperature_2m`
-	)
-		// fetch(
-		// 	`http://maps.googleapis.com/maps/api/geocode/json?address=Miami+FL&sensor=false`
-		// )
-		.then(async response => {
-			const data = await response.json();
-
-			// check for error response
-			if (!response.ok) {
-				// get error message from body or default to response statusText
-				const error = (data && data.message) || response.statusText;
-				return Promise.reject(error);
+		fetch(`https://api.api-ninjas.com/v1/geocoding?city=${city}`, {
+			headers: {
+				'X-Api-Key': 'eTG/xcQHzXjVzmWgPabmsA==KCkIiTK5tQp3bjLo'
 			}
-
-			console.log(`data: `, data);
 		})
-		.catch(error => {
-			this.errorMessage = error;
-			console.error('There was an error!', error);
-		});
-	return {
-		data() {
-			return {
-				test: 'test'
-			};
-		}
-	};
+			.then(async response => {
+				const data = await response.json();
+
+				// check for error response
+				if (!response.ok) {
+					// get error message from body or default to response statusText
+					const error = (data && data.message) || response.statusText;
+					return Promise.reject(error);
+				}
+				lat = data[0].latitude;
+				long = data[0].longitude;
+
+				console.log(`lat: ${lat} / long: ${long}`);
+				res({ lat, long });
+			})
+			.catch(error => {
+				this.errorMessage = error;
+				console.error('There was an error!', error);
+			});
+	});
+};
+
+export const getWeather = city => {
+	// var geocoder = new google.maps.Geocoder();
+	// let lat, long;
+
+	resolveCoords(city).then(coords => {
+		fetch(
+			`https://api.open-meteo.com/v1/forecast?latitude=${coords.lat}&longitude=${coords.long}&hourly=temperature_2m,weathercode&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=America%2FNew_York`
+		)
+			.then(async response => {
+				const data = await response.json();
+
+				// check for error response
+				if (!response.ok) {
+					// get error message from body or default to response statusText
+					const error = (data && data.message) || response.statusText;
+					return Promise.reject(error);
+				}
+
+				console.log(`data: `, data);
+			})
+			.catch(error => {
+				this.errorMessage = error;
+				console.error('There was an error!', error);
+			});
+	});
 };
